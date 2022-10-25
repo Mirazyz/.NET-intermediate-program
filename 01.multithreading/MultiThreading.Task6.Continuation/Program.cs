0fa0a1f3482851bcfcebc50528f8f11e3e218074
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2022-10-25 21:09:39 +0500

    add task 6 implementation

--- 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
+++ 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
@@ -7,6 +7,9 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task6.Continuation
 {
@@ -23,6 +26,100 @@ namespace MultiThreading.Task6.Continuation
             Console.WriteLine();
 
             // feel free to add your code
+
+            var sucessfullTask = new Task(() => Console.WriteLine("Main task"));
+            var faltedTask = new Task(() => throw new Exception());
+            var ts = new CancellationTokenSource();
+            var ct = ts.Token;
+            var cancelledTask = new Task(async () =>
+            {
+                await Task.Delay(100);
+            }, ct);
+
+            // a.
+            sucessfullTask.ContinueWith((parent) =>
+            {
+                Console.WriteLine("Execute regardless of the parent result. Task: success.");
+            });
+
+            faltedTask.ContinueWith((parent) =>
+            {
+                Console.WriteLine("Execute regardless of the parent result. Task: falted.");
+            });
+
+            cancelledTask.ContinueWith((parent) =>
+            {
+                Console.WriteLine("Execute regardless of the parent result. Task: cancelled.");
+            });
+
+            // b.
+            sucessfullTask.ContinueWith((result) =>
+            {
+                Console.WriteLine("Execute when parent falted. Task: success.");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            faltedTask.ContinueWith((result) =>
+            {
+                Console.WriteLine("Execute when parent falted. Task: falted");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            cancelledTask.ContinueWith((result) =>
+            {
+                Console.WriteLine("Execute when parent falted. Task: cancelled");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            // c.
+            sucessfullTask.ContinueWith((result) =>
+            {
+                if (result.IsFaulted)
+                {
+                    Console.WriteLine($"Execute when parent faled in the same thread. Task: success.");
+                }
+            }, ct, TaskContinuationOptions.OnlyOnFaulted, TaskScheduler.Current);
+
+            faltedTask.ContinueWith((result) =>
+            {
+                if (result.IsFaulted)
+                {
+                    Console.WriteLine($"Execute when parent faled in the same thread. Task: falted.");
+                }
+            }, ct, TaskContinuationOptions.OnlyOnFaulted, TaskScheduler.Current);
+
+            cancelledTask.ContinueWith((result) =>
+            {
+                if (result.IsFaulted)
+                {
+                    Console.WriteLine($"Execute when parent faled in the same thread. Task: cancelled.");
+                }
+            }, ct, TaskContinuationOptions.OnlyOnFaulted, TaskScheduler.Current);
+
+            // d.
+            sucessfullTask.ContinueWith((result) =>
+            {
+                Console.WriteLine("Execute when parent is cancelled. Task: success.");
+            }, ct, TaskContinuationOptions.OnlyOnCanceled, TaskScheduler.Default);
+            
+            faltedTask.ContinueWith((result) =>
+            {
+                Console.WriteLine("Execute when parent is cancelled. Task: falted.");
+            }, ct, TaskContinuationOptions.OnlyOnCanceled, TaskScheduler.Default);
+
+            cancelledTask.ContinueWith((result) =>
+            {
+                Console.WriteLine("Execute when parent is cancelled. Task: cancelled.");
+            }, ct, TaskContinuationOptions.OnlyOnCanceled, TaskScheduler.Default);
+
+            
+            sucessfullTask.Start();
+            faltedTask.Start();
+            cancelledTask.Start();
+            ts.CancelAfter(10);
+
+            cancelledTask.Wait();
+
+            Console.WriteLine($"success: {sucessfullTask.Status}");
+            Console.WriteLine($"fail: {faltedTask.Status}");
+            Console.WriteLine($"cancel: {cancelledTask.Status}");
 
             Console.ReadLine();
         }
