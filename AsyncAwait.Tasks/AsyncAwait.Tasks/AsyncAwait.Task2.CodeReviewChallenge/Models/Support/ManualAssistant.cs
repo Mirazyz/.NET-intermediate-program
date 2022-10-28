893ae99e177cf5d319359c294fc28748a5a2f762
Miraziz Khidoyatov <miraziz_khidoyatov@epam.com>
2022-10-28 19:39:50 +0500

    Improve async code

--- AsyncAwait.Tasks/AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Models/Support/ManualAssistant.cs
+++ AsyncAwait.Tasks/AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Models/Support/ManualAssistant.cs
@@ -22,8 +22,7 @@ public class ManualAssistant : IAssistant
             var t = _supportService.RegisterSupportRequestAsync(requestInfo);
             Console.WriteLine(t.Status); // this is for debugging purposes
             Thread.Sleep(5000); // this is just to be sure that the request is registered
-            return await _supportService.GetSupportInfoAsync(requestInfo)
-                .ConfigureAwait(false);
+            return await _supportService.GetSupportInfoAsync(requestInfo);
         }
         catch (HttpRequestException ex)
         {
