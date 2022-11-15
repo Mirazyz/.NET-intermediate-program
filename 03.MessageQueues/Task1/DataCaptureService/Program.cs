90eee1e78ee96ef7b2ec4c00ee07e4457abee3af
Miraziz Khidoyatov <miraziz_khidoyatov@epam.com>
2022-11-15 17:33:24 +0500

    Add sending file over message broker

--- 03.MessageQueues/Task1/DataCaptureService/Program.cs
+++ 03.MessageQueues/Task1/DataCaptureService/Program.cs
@@ -39,5 +39,19 @@ namespace DataCaptureService
 
             Console.WriteLine(message);
         }
+
+        private static void SendFile(byte[] file)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(ServiceConstants.Uri);
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.BasicPublish(ServiceConstants.Exchange, "", null, file);
+
+            channel.Close();
+            connection.Close();
+        }
     }
 }
