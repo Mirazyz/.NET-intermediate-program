48cf1f4c8fb84b02df60a78efb970c43fffde049
Miraziz Khidoyatov <miraziz_khidoyatov@epam.com>
2022-11-15 17:32:28 +0500

    Add directory watching for DataCapture Service

--- 03.MessageQueues/Task1/DataCaptureService/Program.cs
+++ 03.MessageQueues/Task1/DataCaptureService/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+
+namespace DataCaptureService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Processing service has started...");
+
+            Watch();
+
+            Console.ReadLine();
+        }
+
+        private static void Watch()
+        {
+            FileSystemWatcher watcher = new()
+            {
+                Path = ServiceConstants.FolderPath,
+                NotifyFilter = NotifyFilters.LastWrite,
+                Filter = "*.*"
+            };
+
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed) return;
+
+            Console.WriteLine($"Changed: {e.FullPath}");
+        }
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            string message = $"Created: {e.FullPath}";
+
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ 03.MessageQueues/Task1/DataCaptureService/ServiceConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCaptureService
+{
+    public static class ServiceConstants
+    {
+        public const string Uri = "amqp://guest:guest@localhost:5672";
+        public const string Exchange = "DataCaptureExchange";
+        public const string FolderPath = @"C:\RabbitMQ\";
+    }
+}
