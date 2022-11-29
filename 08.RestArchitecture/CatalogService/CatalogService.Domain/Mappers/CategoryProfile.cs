c34e56b9c41dd64694deb820aaf66b24896169bc
Miraziz Khidoyatov <miraziz_khidoyatov@epam.com>
2022-11-29 15:15:58 +0500

    Add Category & Item mappers

---
+++ 08.RestArchitecture/CatalogService/CatalogService.Domain/Mappers/CategoryProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CatalogService.Domain.DTOs.Category;
+using CatalogService.Domain.Entities;
+
+namespace CatalogService.Domain.Mappers
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryDto>();
+            CreateMap<CategoryDto, Category>();
+            CreateMap<CategoryForCreateDto, Category>();
+            CreateMap<CategoryForUpdateDto, Category>();
+        }
+    }
+}
