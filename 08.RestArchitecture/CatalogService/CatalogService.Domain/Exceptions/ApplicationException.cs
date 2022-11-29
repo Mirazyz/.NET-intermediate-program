e4eb0fde2a2f8138e2399d2d40392a45c45b4ec6
Miraziz Khidoyatov <miraziz_khidoyatov@epam.com>
2022-11-29 15:17:38 +0500

    Add domain exceptions

---
+++ 08.RestArchitecture/CatalogService/CatalogService.Domain/Exceptions/ApplicationException.cs
@@ -0,0 +1,10 @@
+﻿namespace CatalogService.Domain.Exceptions
+{
+    public class ApplicationException : Exception
+    {
+        public ApplicationException(string? message)
+            : base(message)
+        {
+        }
+    }
+}
