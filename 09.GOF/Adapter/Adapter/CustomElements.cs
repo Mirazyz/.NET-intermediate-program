c25475b787c33eafc8248df4d0357d3a4d314d02
Miraziz Khidoyatov <miraziz_khidoyatov@epam.com>
2022-12-21 23:24:35 +0500

    Implement adapter

---
+++ 09.GOF/Adapter/Adapter/CustomElements.cs
@@ -0,0 +1,21 @@
+﻿namespace Adapter
+{
+    public class CustomElements : IContainer<int>, IElements<int>
+    {
+        public IEnumerable<int> Items => GetElements();
+
+        public int Count => Items.Count();
+
+        public IEnumerable<int> GetElements()
+        {
+            return new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+        }
+    }
+}
---
+++ 09.GOF/Adapter/Adapter/Program.cs
@@ -0,0 +1,6 @@
+﻿using Adapter;
+
+IContainer<int> container = new CustomElements();
+Printer printer = new Printer();
+
+printer.Print(container);
